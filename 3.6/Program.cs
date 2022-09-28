e1042e18aaec8f5492eeee51657161c27eb14912
vladis555 <vladis555@mail.ru>
2022-09-28 19:07:25 +0300

    3.6 comments removed

--- 3.6/Program.cs
+++ 3.6/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 // Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-/*
+
 bool IsPalindrom(int number) {
     string numberText = Convert.ToString(number);
     return numberText[0]==numberText[4] && numberText[1]==numberText[3];
@@ -11,12 +11,12 @@ Console.Write("Input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("number " + number + " is "+(IsPalindrom(number) ? "" : "not ")+"palindrom");
-*/
+
 
 
 // Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-/*
+
 // Вывод строки и ввод числа
 int ReadInt(string message) {
     Console.Write(message);
@@ -41,7 +41,7 @@ int pointB_Z = ReadInt("Point B coord Z: ");
 double length = GetLength3D (new double[]{pointA_X, pointA_Y, pointA_Z}, new double[]{pointB_X, pointB_Y, pointB_Z});
 
 Console.WriteLine("Distance between two given points is: "+length);
-*/
+
 
 // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
